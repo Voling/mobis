5b05f9930a463336863bba5338def9b7719c82bd
Dylan Vo <dethanvo@gmail.com>
2024-05-25 16:17:26 -0700

    lambda get user success

--- MobisLambda/mobisUserSignup/lambda_function.py
+++ MobisLambda/mobisUserSignup/lambda_function.py
@@ -1,8 +1,56 @@
 import json
+import boto3
+import botocore
+client = boto3.client('dynamodb')
+cognito_client = boto3.client('cognito-idp')
+dynamodb = boto3.resource("dynamodb")
+table = dynamodb.Table('mobisusers')
+
 
 def lambda_handler(event, context):
-    # TODO implement
+    event = json.loads("""{
+    "version": "1",
+    "region": "us-west-2",
+    "userPoolId": "us-west-1_5SOVhTCcK",
+    "userName": "user@example.com",
+    "callerContext": {
+        "awsSdkVersion": "aws-sdk-unknown-unknown",
+        "clientId": "xxxxxxxxxxxx"
+    },
+    "triggerSource": "PostConfirmation_ConfirmSignUp",
+    "request": {
+        "userAttributes": {
+            "sub": "12345678-1234-1234-1234-123456789012",
+            "email_verified": "true",
+            "cognito:user_status": "CONFIRMED",
+            "custom:username": "exampleuser",
+            "email": "user@example.com"
+        }
+    },
+    "response": {}
+    }""")
+    try:
+        userId = event['request']['userAttributes']['sub']  # sub = uuid
+        userPoolId = event['userPoolId']
+        response = cognito_client.admin_get_user(
+            UserPoolId=userPoolId,
+            Username=userId
+        )
+        attributes = {attr['Name']: attr['Value']
+                      for attr in response['UserAttributes']}
+
+    except botocore.exceptions.ClientError as error:
+        return {
+            'statusCode': 500,
+            'body': json.dumps(str(error))
+        }
+    except Exception as e:
+        print(e)
+        return {
+            'statusCode': 404,
+            'body': json.dumps(str(e))
+        }
     return {
         'statusCode': 200,
-        'body': json.dumps(event)
+        'body': json.dumps("hi")
     }
