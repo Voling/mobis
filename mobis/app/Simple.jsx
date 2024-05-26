35b0f6af9492bf19c9b684c9de709037301e04db
Zora Luo <zoraluo@Zoras-MacBook-Pro.local>
2024-05-25 23:21:25 -0700

    ios screen

--- mobis/app/Simple.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { View, Text } from 'react-native';
-export default Simple = () => {
-    return (
-        <View>
-        <Text>Simple</Text>
-        </View>
-    );
-}
