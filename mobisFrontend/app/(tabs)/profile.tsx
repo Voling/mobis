5daa9164c2c952198e209ce5d7bb95468827e8ff
Zora Luo <zoraluo@zoras-mbp.lan>
2024-05-26 04:28:48 -0700

    added profile page

--- mobisFrontend/app/(tabs)/profile.tsx
+++ mobisFrontend/app/(tabs)/profile.tsx
@@ -1,14 +1,34 @@
 import Ionicons from '@expo/vector-icons/Ionicons';
-import { StyleSheet, Text, View, TextInput } from 'react-native';
-// import mobisBG from '../../assets/images/mobisBG.png';
-
+import React from 'react';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity} from 'react-native';
+import { ImageBackground } from 'react-native';
 
 export default function TabTwoScreen() {
+  const handlePress = () => {
+    // Handle button press
+    alert('Button pressed!');
+  };
   return (
-   <View>
-    <TextInput placeholder='User Input' style={styles.textInputStyle}>
-    </TextInput>
-   </View>
+    <ImageBackground
+    source={require('../../assets/svg/background.jsx')}
+    style={styles.container}
+    >
+      <View style={styles.content}>
+        <View style={styles.box}/>
+        <View style={styles.circle}/>
+        <Text style={styles.user}>SuperPlayerGirl</Text>
+        <Text style={styles.games}>Valorant, League of Legends</Text>
+
+        <TextInput placeholder='User Input' style={styles.textInputStyle}/>
+        <TextInput placeholder='User Input2' style={styles.textInputStyle}/>
+        <TextInput placeholder='User Input3' style={styles.textInputStyle}/>
+        <TextInput placeholder='User Input4' style={styles.textInputStyle}/>
+
+        <TouchableOpacity style={styles.button} onPress={handlePress}>
+          <Text style={styles.buttonText}>Update</Text>
+        </TouchableOpacity>
+    </View>
+    </ImageBackground>
   );
 }
 
@@ -26,8 +46,62 @@ const styles = StyleSheet.create({
   textInputStyle: {
     backgroundColor: 'rgb(100,100,100)',
     borderRadius: 10,
-    padding: 10,
-    margin: 10,
-    width: '50%'
+    marginHorizontal: 'auto',
+    height: 60,
+    marginTop: 50,
+    paddingLeft: 15,
+    width: '90%',
+  },
+  box: {
+    backgroundColor: 'rgb(31,30,30)',
+    width: 205,
+    height: 163,
+    marginHorizontal: 'auto',
+    paddingTop: 0,
+    alignItems: 'center',
+  },
+  circle: {
+    backgroundColor: 'gray',
+    width: 124,
+    height: 124,
+    borderRadius: 62,
+    position: 'absolute',
+    top: 15,
+    left: '35%',
+    marginHorizontal: 'auto',
+  },
+  user: {
+    fontSize: 24, 
+    color: 'red',
+    fontWeight: 'bold',
+    marginHorizontal: 'auto',
+    padding: 5,
+  },
+  games: {
+    fontSize: 20, 
+    color: 'red',
+    marginHorizontal: 'auto',
+  },
+  button: {
+    backgroundColor: 'rgb(255, 87, 69)',
+    marginTop: 50,
+    borderRadius: 5,
+    height: 48,
+    width: '85%',
+    marginHorizontal: 'auto',
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 15,
+    textAlign: 'center',
+    paddingVertical: 15,
+  },
+  container: {
+    flex: 1,
+  },
+  content: {
+    padding: 20,
   }
+
 });
---
+++ mobisFrontend/assets/svg/background.jsx
@@ -0,0 +1,35 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<svg id="Layer_2" data-name="Layer 2" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 1135 2440">
+  <defs>
+    <style>
+      .cls-1 {
+        fill: url(#radial-gradient-2);
+      }
+
+      .cls-1, .cls-2, .cls-3 {
+        stroke-width: 0px;
+      }
+
+      .cls-2 {
+        fill: url(#radial-gradient);
+      }
+
+      .cls-3 {
+        fill: #564b4b;
+      }
+    </style>
+    <radialGradient id="radial-gradient" cx="568.5" cy="1220" fx="25.72" fy="-11.3" r="1345.62" gradientUnits="userSpaceOnUse">
+      <stop offset="0" stop-color="#baa4a4"/>
+      <stop offset=".48" stop-color="#baa4a4" stop-opacity="0"/>
+    </radialGradient>
+    <radialGradient id="radial-gradient-2" cx="-798.46" cy="1242.77" fx="-1341.24" fy="11.48" r="1345.62" gradientTransform="translate(1364.96 -22.77)" gradientUnits="userSpaceOnUse">
+      <stop offset="0" stop-color="#baa4a4"/>
+      <stop offset=".48" stop-color="#baa4a4" stop-opacity="0"/>
+    </radialGradient>
+  </defs>
+  <g id="Layer_3" data-name="Layer 3">
+    <rect class="cls-3" x="6" y="2.5" width="1121" height="2437"/>
+  </g>
+  <rect class="cls-2" x="2" y="1.5" width="1133" height="2437"/>
+  <rect class="cls-1" y="1.5" width="1133" height="2437" transform="translate(1133 2440) rotate(180)"/>
+</svg>
